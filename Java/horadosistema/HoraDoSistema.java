4994bf4a3424392e3d6e9723b189d669082316d7
scherquy <jschervensquy@gmail.com>
2026-03-10 11:27:19 -0300

    exercícios em Java

---
+++ Java/horadosistema/HoraDoSistema.java
@@ -0,0 +1,11 @@
+package horadosistema;
+
+import java.util.Date;
+
+public class HoraDoSistema {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+
+        System.out.println("\nA data e a hora do sistema é: " + relogio.toString() + "\n");
+    }
+}
---
+++ Java/idiomadosistema/IdiomaDoSIstema.java
@@ -0,0 +1,11 @@
+package idiomadosistema;
+
+import java.util.Locale;
+
+public class IdiomaDoSIstema {
+    public static void main(String[] args) {
+        Locale idioma = Locale.getDefault();
+
+        System.out.println("\nIdioma do seu S.O: " + idioma.toString() + "\n");
+    }
+}
