424f5375e3c5538e8f450f78ae8cdfac26eef358
scherquy <jschervensquy@gmail.com>
2026-03-13 11:37:09 -0300

    adicionado Java

---
+++ Java/primeiroprograma/src/main/java/primeiroprograma/Primeiroprograma.java
@@ -0,0 +1,11 @@
+package primeiroprograma;
+
+/**
+ *
+ * @author scherquy
+ */
+public class Primeiroprograma {
+    public static void main(String[] args) {
+        System.out.println("\nHello World\n");
+    }
+}
