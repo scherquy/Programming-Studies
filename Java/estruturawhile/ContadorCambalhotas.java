6eb469a64fae0ca3d2dbf8c620196cfb30105c2c
scherquy <jschervensquy@gmail.com>
2026-03-24 11:55:48 -0300

    novos exercícios de Java

---
+++ Java/estruturawhile/ContadorCambalhotas.java
@@ -0,0 +1,15 @@
+package estruturawhile;
+
+public class ContadorCambalhotas {
+    public static void main(String[] args) {
+        
+        int contador = (int) 0;
+
+        while(contador < 4){
+            System.out.printf("\n%dº cambalhota", contador+1);
+            contador++;
+        }
+
+        System.out.printf("\n\n");
+    }
+}
---
+++ Java/fatorial/Fatorial.java
@@ -0,0 +1,23 @@
+package fatorial;
+
+public class Fatorial {
+    public static void main(String[] args) {
+        
+        long factorial = (long) 0;
+        int value = (int) 0;
+
+        while(value <= 12){
+            if(value == 0){
+                factorial = (byte) 1;
+            }   else{
+                    factorial = (long) (factorial * value);
+                }
+
+            System.out.printf("\nFactorial of %d = %d", value, factorial);
+
+            value++;
+        }
+
+        System.out.printf("\n\n");
+    }   
+}
