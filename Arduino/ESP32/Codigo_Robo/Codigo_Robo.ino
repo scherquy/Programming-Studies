3bbb75c83ec1f56ae27f234e6e650819944b3143
scherquy <jschervensquy@gmail.com>
2026-09-19 20:52:45 -0300

    Exercícios em Python

--- Arduino/ESP32/Codigo_Robo/Codigo_Robo.ino
+++ Arduino/ESP32/Codigo_Robo/Codigo_Robo.ino
@@ -1,39 +1,88 @@
-// Definição dos pinos para os dois motores
-const int IN1 = 32; 
-const int IN2 = 33;
-const int IN3 = 27; 
-const int IN4 = 26;
+#define CUSTOM_SETTINGS
+#define INCLUDE_GAMEPAD_MODULE
+#include <DabbleESP32.h>
 
-void setup() {
-  // Configura todos os pinos como saída
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  
-  // Começa com tudo desligado por segurança
+// Motor A
+const int IN1 = 32;
+const int IN2 = 33;
+
+// Motor B
+const int IN3 = 27;
+const int IN4 = 25;
+
+void frente() {
+  // Motor A para frente
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+
+  // Motor B para frente
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void tras() {
+  // Motor A para trás
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  // Motor B para trás
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void direita() {
+  // Gira usando os dois motores
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void esquerda() {
+  // Oposto da direita
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void parar() {
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
+
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, LOW);
 }
 
-void loop() {
-  // --- ANDAR PARA FRENTE ---
-  // Motor A liga
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  // Motor B liga
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  
-  delay(3000); // Anda por 3 segundos
+void setup() {
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 
-  // --- PARAR ---
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-  
-  delay(3000); // Fica parado por 3 segundos
+  parar();
+
+  Dabble.begin("Carrinho_Manna_ESP32");
+}
+
+void loop() {
+  Dabble.processInput();
+
+  if (GamePad.isCrossPressed()) {
+    tras();
+  }
+  else if (GamePad.isSquarePressed()) {
+    frente();
+  }
+  else if (GamePad.isRightPressed()) {
+    direita();
+  }
+  else if (GamePad.isLeftPressed()) {
+    esquerda();
+  }
+  else {
+    parar();
+  }
 }
--- Python/Aprendendo Python/testes.py
+++
@@ -1,7 +0,0 @@
-notas = [6.0, 7.0, 9.0, 5.5, 8.0]
-qualitativo = 0.5
-
-notaFinal = map(lambda nota: nota + qualitativo, notas)
-notaFinal = list(notaFinal)
-
-print(notaFinal)
